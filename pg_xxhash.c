d66dfc518c7c13ea70ca39e0a6bf5e637be874d5
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-08-02 10:26:03 -0300

    Simplified a bit removing unecessary code
    Signed-off-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

--- pg_xxhash.c
+++ pg_xxhash.c
@@ -19,23 +19,16 @@
 
 PG_MODULE_MAGIC;
 
-static int64
-xxhash64_internal(char *input, int64 seed)
-{
-	return (int64) XXH64(input, strlen(input), (XXH64_hash_t) seed);
-}
-
-PG_FUNCTION_INFO_V1(pg_xxhash64);
-
 /*
  * xxHash64 function: https://cyan4973.github.io/xxHash/
  */
+PG_FUNCTION_INFO_V1(pg_xxhash64);
+
 Datum
 pg_xxhash64(PG_FUNCTION_ARGS)
 {
 	char *input = text_to_cstring(PG_GETARG_TEXT_P(0));
 	int64 seed = PG_ARGISNULL(1) ? 0 : PG_GETARG_INT64(1);
 
-	PG_RETURN_INT64(xxhash64_internal(input, seed));
+	PG_RETURN_INT64((int64) XXH64(input, strlen(input), (XXH64_hash_t) seed));
 }
-
