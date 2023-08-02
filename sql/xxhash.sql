3fb24f08afc2ebf804b99fea58f2eeef24cef6f2
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-08-02 11:14:20 -0300

    Added pg_xxh3_64 function
    Signed-off-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

--- sql/xxhash.sql
+++ sql/xxhash.sql
@@ -3,6 +3,7 @@
 \pset null 'NULL'
 CREATE EXTENSION pg_xxhash;
 
+-- XXH64 Tests
 -- All tests below should return NULL
 SELECT pg_xxhash64(NULL);
 SELECT pg_xxhash64(NULL, NULL);
@@ -21,5 +22,12 @@ SELECT pg_xxhash64('foo') IS DISTINCT FROM pg_xxhash64('foo', NULL); -- should b
 SELECT pg_xxhash64('foo', 100);
 SELECT pg_xxhash64('foo', -100);
 SELECT pg_xxhash64('foo', 100) = pg_xxhash64('foo', -100); -- should be FALSE
+
+-- XXH3_64bits tests
+SELECT pg_xxh3_64(NULL); -- should return NULL
+SELECT pg_xxh3_64('foo');
+SELECT pg_xxh3_64('bar');
+SELECT pg_xxh3_64('foo') = pg_xxh3_64('foo'); -- should be TRUE
+SELECT pg_xxh3_64('foo') = pg_xxh3_64('bar'); -- should be FALSE
 
 DROP EXTENSION pg_xxhash;
